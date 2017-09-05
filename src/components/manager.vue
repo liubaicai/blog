91b7630ef57eabe9801227f27692621e37c4d097
刘白菜 <liushuai.baicai@hotmail.com>
2017-09-05 14:22:23 +0800

    update ui

--- src/components/manager.vue
+++ src/components/manager.vue
@@ -1,10 +1,10 @@
 <template>
   <div style="margin-top: 30px;">
     <ul class="nav nav-tabs">
-      <li role="presentation" class="active"><a href="#">文章管理</a></li>
-      <li role="presentation"><a href="#">分类管理</a></li>
-      <li role="presentation"><a href="#">链接管理</a></li>
-      <li role="presentation"><a href="#">网站设置</a></li>
+      <li role="presentation" class="active"><a>文章管理</a></li>
+      <li role="presentation"><a>分类管理</a></li>
+      <li role="presentation"><a>链接管理</a></li>
+      <li role="presentation"><a>网站设置</a></li>
     </ul>
   </ul>
   </div>
@@ -22,5 +22,7 @@
 </script>
 
 <style scoped>
-
+  a {
+    color: #999999;
+  }
 </style>
