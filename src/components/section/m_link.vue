d3127692bd9c09879a0045ed18665ff91cb612a0
Baicai <liushuai.baicai@hotmail.com>
2017-09-06 23:16:51 +0800

    链接编辑

--- src/components/section/m_link.vue
+++ src/components/section/m_link.vue
@@ -6,17 +6,26 @@
     <table class="table table-striped" style="margin-top: 10px;">
       <tbody>
       <tr>
-        <th>#</th>
-        <th>Title</th>
-        <th>Url</th>
-        <th colspan="2"></th>
+        <th style="width: 10%;">#</th>
+        <th style="width: 30%;">Title</th>
+        <th style="width: auto;">Url</th>
+        <th style="width: 15%;" colspan="2"></th>
       </tr>
       <tr v-for="(link, index) in links">
-        <td>{{link.sort}}</td>
-        <td>{{link.title}}</td>
-        <td>{{link.url}}</td>
-        <td><a>编辑</a></td>
-        <td><a @click.prevent="deleteLink(index, link.id)">删除</a></td>
+        <template v-if="link.editing === true">
+          <td><input type="text" v-model="link.sort" class="form-control"></td>
+          <td><input type="text" v-model="link.title" class="form-control"></td>
+          <td><input type="text" v-model="link.url" class="form-control"></td>
+          <td><a @click.prevent="onEditLinkSubmit(index, link)">保存</a></td>
+          <td><a @click.prevent="link.editing = false">取消</a></td>
+        </template>
+        <template v-else>
+          <td><span>{{link.sort}}</span></td>
+          <td><span>{{link.title}}</span></td>
+          <td><span>{{link.url}}</span></td>
+          <td><a @click.prevent="link.editing = true">编辑</a></td>
+          <td><a @click.prevent="onDeleteLinkClick(index, link.id)">删除</a></td>
+        </template>
       </tr>
       </tbody>
     </table>
@@ -34,7 +43,11 @@
         document.title = this.$default_title
         var that = this
         this.getLinks().then(function (data) {
-          that.links = data['data']
+          var result = data['data']
+          for (var i = 0; i < result.length; i++) {
+            data['data'][i].editing = false
+          }
+          that.links = result
         })
       },
       methods: {
@@ -42,11 +55,31 @@
           var that = this
           var sendData = {link: {title: '标题', url: '地址', sort: 99}, token: this.$cookie.get('admin_authorization')}
           this.toNewLink(sendData).then(function (data) {
-            that.links.unshift(data['data'])
-            that.links.sort(that.sortBy('sort', false))
+            if (data['code'] === 200) {
+              var result = data['data']
+              result.editing = false
+              that.links.unshift(result)
+              that.links.sort(that.sortBy('sort', false))
+            } else {
+              that.$alert(data['message'])
+            }
           })
         },
-        deleteLink (index, id) {
+        onEditLinkSubmit (index, link) {
+          var that = this
+          var sendData = {link: {id: link.id, title: link.title, url: link.url, sort: link.sort}, token: this.$cookie.get('admin_authorization')}
+          this.toEditLink(link.id, sendData).then(function (data) {
+            if (data['code'] === 200) {
+              var result = data['data']
+              result.editing = false
+              that.links.splice(index, 1, result)
+              that.links.sort(that.sortBy('sort', false))
+            } else {
+              that.$alert(data['message'])
+            }
+          })
+        },
+        onDeleteLinkClick (index, id) {
           var that = this
           that.$confirm('确定删除?')
             .then(function () {
@@ -86,5 +119,8 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+  span, a, input {
+    height: 26px;
+    line-height: 26px;
+  }
 </style>
